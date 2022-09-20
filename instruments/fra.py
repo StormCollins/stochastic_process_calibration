6c9f8c3bdfb5bf2c3c704e0fda33ad6f70779d90
Storm Collins <stcollins@deloitte.co.za>
2022-09-20 11:43:18 +0200

    Minor refactoring.

--- instruments/fra.py
+++ instruments/fra.py
@@ -8,7 +8,7 @@ class Fra:
         self.start_tenor: float = start_tenor
         self.end_tenor: float = end_tenor
 
-    def get_monte_carlo_value(
+    def get_monte_carlo_values(
             self,
             alpha: float,
             sigma: float,
@@ -16,21 +16,27 @@ class Fra:
             number_of_paths: int,
             number_of_time_steps: int,
             short_rate_tenor: float = 0.01):
+        dt = self.start_tenor / number_of_time_steps
         hw: HullWhite = HullWhite(alpha, sigma, curve, short_rate_tenor)
+
         tenors, short_rates = \
             hw.simulate(self.start_tenor, number_of_paths, number_of_time_steps, SimulationMethod.SLOWANALYTICAL)
+
+        # discount_factors = \
+        #     hw.a_function(self.start_tenor, np.array([self.end_tenor])) * \
+        #     np.exp(-1 * short_rates[:, -1] * hw.b_function(self.start_tenor, np.array([self.end_tenor])))
         discount_factors = \
             hw.a_function(self.start_tenor, np.array([self.end_tenor])) * \
-            np.exp(-1 * short_rates[:, -1] * hw.b_function(self.start_tenor, np.array([self.end_tenor])))
+            np.exp(-1 * short_rates * hw.b_function(self.start_tenor, np.array([self.end_tenor])))
 
         forward_rates = (1/(self.end_tenor - self.start_tenor)) * ((1 / discount_factors) - 1)
-        dt = self.start_tenor / number_of_time_steps
-        stochastic_discount_factors = np.prod(np.exp(-1 * short_rates * dt), 1)
+        stochastic_dfs = np.cumprod(np.exp(-1 * short_rates * dt), 1)
+
         return np.mean(
             self.notional *
             (forward_rates - self.strike) *
             (self.end_tenor - self.start_tenor) *
-            stochastic_discount_factors)
+            stochastic_dfs, 0)
 
     def get_fair_forward_rate(self, curve: Curve):
         return curve.get_forward_rates(
--- tests-instruments/tests_fra.py
+++ tests-instruments/tests_fra.py
@@ -23,20 +23,16 @@ def atm_fra(flat_curve):
     return Fra(notional, strike, start_tenor, end_tenor)
 
 
-def test_get_monte_carlo_value_compared_to_monte_carlo(flat_curve):
-    notional: float = 1_000_000
+def test_get_monte_carlo_values(flat_curve, atm_fra):
     strike: float = 0.10126
     alpha: float = 0.1
     sigma: float = 0.1
     number_of_paths: int = 100_000
     number_of_time_steps: int = 100
-    start_tenor = 1
-    end_tenor = 1.25
-    fra: Fra = Fra(notional, strike, start_tenor, end_tenor)
 
     np.random.seed(999)
-    actual: float = \
-        fra.get_monte_carlo_value(
+    actual: np.ndarray = \
+        atm_fra.get_monte_carlo_values(
             alpha=alpha,
             sigma=sigma,
             curve=flat_curve,
@@ -47,21 +43,25 @@ def test_get_monte_carlo_value_compared_to_monte_carlo(flat_curve):
     hw: HullWhite = HullWhite(alpha, sigma, flat_curve, short_rate_tenor)
     np.random.seed(999)
     tenors, short_rates = \
-        hw.simulate(start_tenor, number_of_paths, number_of_time_steps, SimulationMethod.SLOWANALYTICAL)
+        hw.simulate(atm_fra.start_tenor, number_of_paths, number_of_time_steps, SimulationMethod.SLOWANALYTICAL)
 
-    dt = start_tenor / number_of_time_steps
+    dt = atm_fra.start_tenor / number_of_time_steps
     stochastic_discount_factors = np.prod(np.exp(-1 * short_rates * dt), 1)
 
     df = \
-        hw.a_function(start_tenor, np.array([end_tenor])) * \
-        np.exp(-1 * short_rates[:, -1] * hw.b_function(start_tenor, np.array([end_tenor])))
+        hw.a_function(atm_fra.start_tenor, np.array([atm_fra.end_tenor])) * \
+        np.exp(-1 * short_rates[:, -1] * hw.b_function(atm_fra.start_tenor, np.array([atm_fra.end_tenor])))
 
-    forward_rates = (1/(end_tenor - start_tenor)) * ((1/df) - 1)
+    forward_rates = (1/(atm_fra.end_tenor - atm_fra.start_tenor)) * ((1/df) - 1)
 
     expected: float = \
-        float(np.mean(notional * (forward_rates - strike) * (end_tenor - start_tenor) * stochastic_discount_factors))
+        float(np.mean(
+            atm_fra.notional *
+            (forward_rates - strike) *
+            (atm_fra.end_tenor - atm_fra.start_tenor) *
+            stochastic_discount_factors))
 
-    assert actual == pytest.approx(expected, 0.0001)
+    assert actual[-1] == pytest.approx(expected, 0.0001)
 
 
 def test_get_fair_forward_rate(flat_curve, atm_fra):
@@ -78,3 +78,7 @@ def test_get_value(flat_curve, atm_fra):
          atm_fra.strike) * \
         (atm_fra.end_tenor - atm_fra.start_tenor)
     assert actual == pytest.approx(expected, 0.0001)
+
+
+def test_get_monte_carlo_value_compared_to_analytical(flat_curve, atm_fra):
+    return 0
