48f637d620ff7c11dddd6df0900cfb9e261312ec
Storm Collins <stcollins@deloitte.co.za>
2022-10-03 15:24:17 +0200

    Minor refactoring.

--- src/gbm/time_dependent_gbm.py
+++ src/gbm/time_dependent_gbm.py
@@ -1,7 +1,7 @@
 import numpy as np
-from scipy.stats import norm
-from scipy.interpolate import interp1d
 import pandas as pd
+from scipy.interpolate import interp1d
+from scipy.stats import norm
 
 
 class TimeDependentGBM:
