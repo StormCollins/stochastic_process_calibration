fc011ba6e64bce2ca2a50a3484c58da429e0b686
Storm Collins <stcollins@deloitte.co.za>
2022-10-19 16:08:25 +0200

    Created fixture 'excel_file_path' in test_european_equity_option.

--- tests/test_time_dependent_gbm.py
+++ tests/test_time_dependent_gbm.py
@@ -8,11 +8,14 @@ from src.gbm.time_independent_gbm import TimeIndependentGBM
 from src.enums_and_named_tuples.path_statistics import PathStatistics
 
 
-def test_get_time_dependent_volatility_for_constant_vol():
+@pytest.fixture
+def excel_file_path() -> str:
+    return r'tests/equity-atm-volatility-surface.xlsx'
+
+
+def test_get_time_dependent_volatility_for_constant_vol(excel_file_path):
     drift: float = 0.1
     volatility: float = 0.4
-    excel_file_path = r'tests/equity-atm-volatility-surface.xlsx'
-
     gbm: TimeDependentGBM = \
         TimeDependentGBM(
             drift=drift,
@@ -27,11 +30,9 @@ def test_get_time_dependent_volatility_for_constant_vol():
     assert actual_vols == pytest.approx(expected_vols, abs=0.00001)
 
 
-def test_get_time_dependent_volatility_for_non_constant_vol():
+def test_get_time_dependent_volatility_for_non_constant_vol(excel_file_path):
     drift: float = 0.1
     volatility: float = 0.4
-    excel_file_path = r'tests/equity-atm-volatility-surface.xlsx'
-
     gbm: TimeDependentGBM = \
         TimeDependentGBM(
             drift=drift,
@@ -46,16 +47,14 @@ def test_get_time_dependent_volatility_for_non_constant_vol():
     assert actual_vols == pytest.approx(expected_vols, abs=0.00001)
 
 
-def test_get_time_dependent_gbm_paths_for_constant_vols():
+def test_get_time_dependent_gbm_paths_for_constant_vols(excel_file_path):
     drift: float = 0.1
     volatility: float = 0.4
-    excel_file_path = 'tests/equity-atm-volatility-surface.xlsx'
     number_of_paths: int = 10
     number_of_time_steps: int = 2
     initial_spot: float = 50
     time_to_maturity = 1
     np.random.seed(999)
-
     time_dependent_gbm: TimeDependentGBM = \
         TimeDependentGBM(
             drift=drift,
@@ -75,18 +74,21 @@ def test_get_time_dependent_gbm_paths_for_constant_vols():
         TimeIndependentGBM(drift=drift, volatility=volatility, initial_spot=initial_spot)
 
     np.random.seed(999)
-    expected_paths = time_independent_gbm.get_paths(number_of_paths=number_of_paths,
-                                                    number_of_time_steps=number_of_time_steps,
-                                                    time_to_maturity=time_to_maturity)
+    expected_paths = \
+        time_independent_gbm.get_paths(
+            number_of_paths=number_of_paths,
+            number_of_time_steps=number_of_time_steps,
+            time_to_maturity=time_to_maturity)
+
     assert actual_paths == pytest.approx(expected_paths, abs=1.0)
 
 
-def test_distribution():
+def test_distribution(excel_file_path):
     np.random.seed(999)
     time_to_maturity: float = 1.0
     gbm: TimeDependentGBM = \
         TimeDependentGBM(drift=0.0,
-                         excel_file_path='tests/equity-atm-volatility-surface.xlsx',
+                         excel_file_path=excel_file_path,
                          sheet_name='constant_vol_surface',
                          initial_spot=100)
 
@@ -97,9 +99,8 @@ def test_distribution():
     assert path_stats.EmpiricalStandardDeviation == pytest.approx(path_stats.TheoreticalStandardDeviation, abs=1.00)
 
 
-def test_bootstrapped_vols_for_non_constant_vol_term_structure():
+def test_bootstrapped_vols_for_non_constant_vol_term_structure(excel_file_path):
     drift: float = 0.1
-    excel_file_path = r'tests/equity-atm-volatility-surface.xlsx'
     gbm: TimeDependentGBM = \
         TimeDependentGBM(
             drift=drift,
@@ -108,8 +109,34 @@ def test_bootstrapped_vols_for_non_constant_vol_term_structure():
             initial_spot=0)
 
     np.random.seed(999)
-    tenors = [0.0000, 0.0833, 0.1667, 0.2500, 0.5000, 0.7500, 1.0000, 2.0000, 3.0000, 5.0000, 7.0000, 10.0000]
-    tenors = [t - 0.0001 for t in tenors]
-    actual = [float(gbm.get_time_dependent_vol(t)) for t in tenors]
-    expected = [0.12775, 0.12775, 0.13575, 0.14942, 0.15085, 0.15242, 0.15492, 0.16267, 0.15500, 0.16721, 0.16035, 0.14716]
-    assert actual == pytest.approx(expected, abs=0.00001)
+    tenors: list[float] = \
+        [0.0000,
+         0.0833,
+         0.1667,
+         0.2500,
+         0.5000,
+         0.7500,
+         1.0000,
+         2.0000,
+         3.0000,
+         5.0000,
+         7.0000,
+         10.0000]
+
+    tenors: list[float] = [t - 0.0001 for t in tenors]
+    actual: list[float] = [float(gbm.get_time_dependent_vol(t)) for t in tenors]
+    expected: list[float] = \
+        [0.12775,
+         0.12775,
+         0.13575,
+         0.14942,
+         0.15085,
+         0.15242,
+         0.15492,
+         0.16267,
+         0.15500,
+         0.16721,
+         0.16035,
+         0.12827]
+
+    assert actual == pytest.approx(expected, abs=0.001)
